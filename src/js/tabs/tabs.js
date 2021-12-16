a5da6b55f1c90678b990741a182886e79925e3b9
Salgary <kdp160575@gmail.com>
2021-12-17 02:06:26 +0300

    tab-pills

--- src/js/tabs/tabs.js
+++ src/js/tabs/tabs.js
@@ -1,25 +1,25 @@
-// const loginCard = document.getElementsByClassName(".login-tab");
-// const signCard = document.getElementsByClassName(".sign-tab");
-
 const loginCard = document.getElementById('login-tab-id');
 const signCard = document.getElementById('sign-tab-id');
+const getLogin = document.getElementById('login');
+const getTab = document.getElementById('sign');
 
 export function viewSign() {
-    // const loginCard = document.getElementById('login-tab-id');
-    // const signCard = document.querySelector(".sign-tab");
-    console.log(loginCard);
-    console.log(signCard);
+    console.log(getLogin);
+    console.log(getTab);
     loginCard.classList.add('login-tab');
     signCard.classList.remove('sign-tab');
+    getTab.classList.add('active');
+    getLogin.classList.remove('active');
 }
 
 export function viewLogin() {
-    // const signCard = document.getElementById('sign-tab-id');
-    // const signCard = document.querySelector(".sign-tab");
-    console.log(loginCard);
-    console.log(signCard);
+    console.log(getLogin);
+    console.log(getTab);
     loginCard.classList.remove('login-tab');
     signCard.classList.add('sign-tab');
+    getLogin.classList.add('active');
+    getTab.classList.remove('active');
+
 }
 
 
